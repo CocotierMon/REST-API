f3052213b62af4baae1b53235f33b53ae2df030f
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-11-24 11:03:34 +0100

    Aktualizacja na Heroku

--- src/main/java/com/crud/tasks_api/TasksApiApplication.java
+++ src/main/java/com/crud/tasks_api/TasksApiApplication.java
@@ -2,18 +2,17 @@ package com.crud.tasks_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApiApplication extends SpringBootServletInitializer {
+public class TasksApiApplication {
+        //extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApiApplication.class, args);
     }
 
-    @Override
+   /* @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TasksApiApplication.class);
-    }
+    }*/
 }

73f3f32d108060a37c92eadb68f783190b75d886
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-11-25 12:56:06 +0100

    First attempt to Heroku

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,59 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.dto.TrelloBoardDto;
+import com.crud.tasks.dto.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createNewCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+}
+
+       /* if (boardsConditions()) {
+            List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+            trelloBoards.forEach(trelloBoardDto -> {
+
+                System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+
+                System.out.println("This board contains lists: ");
+
+                trelloBoardDto.getLists().forEach(trelloList ->
+                        System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+            });*/
+
+
+   /* private boolean boardsConditions() {
+        List<TrelloBoardDto> trelloBoardsWithConditions = trelloClient.getTrelloBoards();
+
+        for (TrelloBoardDto trelloBoardDto : trelloBoardsWithConditions) {
+            if (trelloBoardDto.getName().contains("Kodilla")
+                    && trelloBoardDto.getId() != null
+                    && trelloBoardDto.getName() != null) {
+                return true;
+            }
+            System.out.println("Brak listy, ktora spełnia warunki");
+            return false;
+        }
+        return false;
+    }*/
--- src/main/java/com/crud/tasks_api/controller/TaskNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.crud.tasks_api.controller;
-
-public class TaskNotFoundException extends Exception {
-}

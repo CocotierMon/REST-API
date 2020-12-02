34fdcd43933292440faef4ba8fcce3f778c8b06b
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-12-02 22:36:58 +0100

    Pobieranie wartości pola Cc

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -24,13 +24,14 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test message");
+        Mail mail = new Mail("test@test.com", "Test", "Test message", "test@test.com");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         mailMessage.setCc(mail.getToCc());
+        System.out.println(mail.getToCc());
 
         //When
         simpleEmailService.send(mail);

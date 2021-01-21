cc506c66d2d74871592bdb6953514d9480d699da
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2021-01-21 11:03:55 +0100

    aktualizacja

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,7 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "* * * 1 * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String ifOneTask = (size == 1) ? " task" : " tasks";

a65706fde64624f18840e7a0636b0f806d4e5dd6
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2021-01-25 14:36:01 +0100

    refaktoryzacja

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,25 +10,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
-
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
 
     @Autowired
-    TrelloFacade trelloFacade;
+    private TrelloFacade trelloFacade;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
+    @RequestMapping(method = RequestMethod.GET, value = "/boards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloFacade.fetchTrelloBoards();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/createTrelloCard")
+    @RequestMapping(method = RequestMethod.POST, value = "/cards")
     public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloFacade.createdCard(trelloCardDto);
     }
-
-
 }
 
        /* if (boardsConditions()) {
